89614c062e6645b879fa23612272441516c81ed5
Forrest Morrison <forrestmorrison@gmail.com>
2024-01-11 19:10:25 -0600

    created drums array & cycleDrums function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
-function toggleStep(e) {
+const toggleStep = (e) => {
     var color = "lightgreen";
     if (e.style.color === color) {
         e.style.color = "black"
     } else {
         e.style.color = color;
     }   
+}
+
+const drums = ["BD", "SD", "LT", "MT", "HT", "RS", "HC", "CB", "CY", "OH", "CH"];
+
+let initialDrumVal = drums[0];
+
+const cycleDrums = (cur) => {
+    for (let i = 0; i < drums.length; i++) {
+        if(cur[i] === (drums.length - 1)) {
+            cur[i] = 0;
+        } else {
+            i++;
+        }
+    }
 }
